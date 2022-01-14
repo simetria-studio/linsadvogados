3d2b5d1ca4a2dc14759174e6b8c49c7731c9bc8d
Felipe cruz <felipephplow@gmail.com>
2022-01-13 21:56:42 -0300

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,6 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <a href="{{ route('financeiro') }}" class="col-lg-3">
-                <div class="card text-white bg-primary iq-mb-3">
-                    <div class="card-body">
-                        <h4 class="card-title text-white">Financeiro</h4>
-                        <blockquote class="blockquote mb-0">
-                            <p class="font-size-14">Controle seus assuntos financeiro.</p>
-                        </blockquote>
-                    </div>
-                </div>
-            </a>
             <a href="{{ route('clientes') }}" class="col-lg-3">
                 <div class="card text-white bg-secondary iq-mb-3">
                     <div class="card-body">
@@ -22,6 +12,17 @@
                     </div>
                 </div>
             </a>
+            <a href="{{ route('financeiro') }}" class="col-lg-3">
+                <div class="card text-white bg-primary iq-mb-3">
+                    <div class="card-body">
+                        <h4 class="card-title text-white">Financeiro</h4>
+                        <blockquote class="blockquote mb-0">
+                            <p class="font-size-14">Controle seus assuntos financeiro.</p>
+                        </blockquote>
+                    </div>
+                </div>
+            </a>
+
             <a href="{{ route('pericia') }}" class="col-lg-3">
                 <div class="card text-white bg-success iq-mb-3">
                     <div class="card-body">
@@ -42,7 +43,26 @@
                     </div>
                 </div>
             </a>
-
+            <a href="{{ route('prazos') }}" class="col-lg-3">
+                <div class="card text-white bg-primary iq-mb-3">
+                    <div class="card-body">
+                        <h4 class="card-title text-white">Prazos a cumprir</h4>
+                        <blockquote class="blockquote mb-0">
+                            <p class="font-size-14">Controle os prazos a cumprir.</p>
+                        </blockquote>
+                    </div>
+                </div>
+            </a>
+            <a href="{{ route('processos') }}" class="col-lg-3">
+                <div class="card text-white bg-secondary iq-mb-3">
+                    <div class="card-body">
+                        <h4 class="card-title text-white">Processos arquivados</h4>
+                        <blockquote class="blockquote mb-0">
+                            <p class="font-size-14">Veja, seus processos arquivados.</p>
+                        </blockquote>
+                    </div>
+                </div>
+            </a>
 
         </div>
     </div>
